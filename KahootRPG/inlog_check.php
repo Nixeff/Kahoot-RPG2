15bb04c146c6cf9262af17c4cc102edbd3d64f58
Nixeff <theodore.folkesson@gmail.com>
2022-09-05 15:25:05 +0200

    Jag kan läsa och skriva upp frågor har också slump

---
+++ KahootRPG/inlog_check.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kahootrpg";
+$data = [];
+
+
+function connCheck($conne, $sqll) {
+  if ($conne->query($sqll) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sqll . "<br>" . $conne->error;
+  }
+}
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
